56246caf2cb90f93e0c329b09f61f76091f06779
Miguel Angel Tadakuma <tadakumamiguelangel@gmail.com>
2022-04-19 19:56:35 -0300

    agregado de NavBar

---
+++ proyecto-final/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+import logo from './logo.png';
+
+function NavBar(props) {
+    return (
+        <div className='nav'>
+            <img src={logo} alt='logo' className='nav-logo'/>
+            <ul className='nav-menu'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Productos</a></li>
+                <li><a href='#'>Nosotros</a></li>
+                <li><a href='#'>Contacto</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
