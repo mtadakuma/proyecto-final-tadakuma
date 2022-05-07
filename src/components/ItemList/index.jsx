e82936061552c4381a5771bf37f1b1a064859186
Miguel Angel Tadakuma <tadakumamiguelangel@gmail.com>
2022-05-07 20:20:08 -0300

    modified prop name on ItemList

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Item from '../Item'
 
 
-const ItemList = ({ itemList }) => {
+const ItemList = ({ items }) => {
     return (
-        itemList?.map((item, key) => <Item item={item} key={key}/>)
+        items?.map((item, key) => <Item item={item} key={key}/>)
     )
 }
 
